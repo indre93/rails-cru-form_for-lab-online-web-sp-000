59eb703ecf21123ea275ab861921d0fb4182ed49
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-22 03:48:04 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -20,8 +20,8 @@ class SongsController < ApplicationController
 
   def create
     @song = Song.new(song_params)
-    @song.artist_id = Artist.find_or_create_by(params)
-    @song.genre_id = Genre.find_or_create_by(id: params[:id])
+    @song.artist_id = Artist.find_or_create_by(name: params[:name])
+    @song.genre_id = Genre.find_or_create_by(name: params[:name])
     @song.save
     redirect_to song_path(@song)
   end
