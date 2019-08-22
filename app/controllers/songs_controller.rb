1cbe9cfbcea2342dc61e83a99930f10b3f729bfe
Indre Bakeer <indre.bakeer@gmail.com>
2019-08-22 02:11:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,33 @@
 class SongsController < ApplicationController
+
+  def show
+    @artist = Song.find(params[:id])
+  end
+
+  def new
+    @artist = Song.new
+  end
+
+  def edit
+    @artist = Song.find(params[:id])
+  end
+
+  def create
+    @artist = Song.new(song_params)
+    @artist.save
+    redirect_to song_path(@song)
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit!
+  end
+
 end
